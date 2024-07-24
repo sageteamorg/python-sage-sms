43547887331181401b454554eb1efbef437b4b41
MohmdFo <mohammad.fotouhi80@gmail.com>
2024-07-24 17:55:34 +0330

    refactor(BackendModuleLoader): add discover_backends to load dynamicly backends

--- sage_sms/factory.py
+++ sage_sms/factory.py
@@ -1,62 +1,35 @@
-"""
-SMS Backend Loading and Factory Module
-
-This module contains classes responsible for loading the appropriate SMS
-backend modules and creating instances of those backends.
-
-Classes:
-    - BackendModuleLoader: Class for loading SMS backend modules by provider name.
-    - SMSBackendFactory: Factory class for creating instances of SMS backends.
-
-Exceptions:
-    - ImportError: Raised when the required package for a provider is not installed.
-
-Usage:
-    Use `BackendModuleLoader.load_backend_module` to load a backend module.
-    Use `SMSBackendFactory.get_backend` to get an instance of the appropriate backend.
-
-Example:
-    >>> backend = SMSBackendFactory.get_backend(True, False, 'sms_ir')
-"""
+import pkgutil
+import inspect
 from importlib import import_module
 from typing import ClassVar, Dict
 
+from .utils import load_settings
 from .backend.base import ConsoleSMSBackend
+from .design.interfaces.provider import ISmsProvider
 
 
 class BackendModuleLoader:
-    """
-    BackendModuleLoader Class
+    _cached_backends: ClassVar[Dict[str, type]] = {}
+    _provider_classname_map: ClassVar[Dict[str, type]] = {}
 
-    Responsible for dynamically loading SMS backend modules given a provider name.
+    @classmethod
+    def discover_backends(cls, base_package: str):
+        backend_package = import_module(base_package)
+        for _, module_name, _ in pkgutil.iter_modules(backend_package.__path__):
+            module = import_module(f"{base_package}.{module_name}")
+            for name, obj in inspect.getmembers(module, inspect.isclass):
+                # Check if the class implements ISmsProvider and is not ISmsProvider itself
+                if issubclass(obj, ISmsProvider) and obj is not ISmsProvider:
+                    cls._provider_classname_map[module_name] = obj.__name__
+                    break
 
-    Attributes:
-        _cached_backends (dict): A dictionary to cache loaded backend classes.
-        _provider_classname_map (dict): A mapping from provider name
-        to backend class name.
-    """
-
-    _cached_backends: ClassVar[Dict[str, str]] = {}
-    _provider_classname_map: ClassVar[Dict[str, str]] = {
-        "sms_ir": "SmsIr",
-        "kavenegar": "Kavenegar",
-    }
+    @staticmethod
+    def _get_class_name(module_name: str) -> str:
+        # Convert the module name to CamelCase
+        return ''.join(word.capitalize() for word in module_name.split('_'))
 
     @staticmethod
     def load_backend_module(provider: dict):
-        """
-        Load a backend module given the provider name.
-
-        Args:
-            provider (dict): The name of the SMS provider.
-
-        Returns:
-            class: The SMS backend class corresponding to the provider.
-
-        Raises:
-            ImportError: Raised when the required package
-            for the provider is not installed.
-        """
         provider_name = provider.get("NAME")
 
         if not provider_name:
@@ -68,57 +41,16 @@ class BackendModuleLoader:
         if provider_name in BackendModuleLoader._cached_backends:
             return BackendModuleLoader._cached_backends[provider_name]
 
-        class_name = BackendModuleLoader._provider_classname_map.get(
-            provider_name, f"{provider_name.capitalize()}Backend"
-        )
-
-        try:
-            module = import_module(f"account.communication.backend.{provider_name}")
-            backend_class = getattr(module, class_name)
-            BackendModuleLoader._cached_backends[provider_name] = backend_class
-            return backend_class
-        except ImportError as err:
-            raise ImportError(
-                f"You must install the package for {provider_name}."
-            ) from err
+        provider_class = BackendModuleLoader._provider_classname_map[provider_name]
+        BackendModuleLoader._cached_backends[provider_name] = provider_class
+        return provider_class
 
 
 class SMSBackendFactory:
-    """
-    SMSBackendFactory Class
-
-    Factory class for creating instances of various SMS backends.
-
-    Methods:
-        - get_backend: Get an instance of the appropriate SMS backend.
-    """
-
     @staticmethod
     def get_backend(settings: dict, *args, **kwargs):
-        """
-        Get an instance of the appropriate SMS backend
-        based on settings and provider name.
+        if settings.get("debug", False):
+            return ConsoleSMSBackend
 
-        Args:
-            is_sms_debug_enabled (bool): Flag to enable debug mode.
-            provider (str): The name of the SMS provider.
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-
-        Returns:
-            object: An instance of the appropriate SMS backend class.
-        """
-
-        if (debug := settings.get("debug")) is not None:
-            if debug:
-                return ConsoleSMSBackend()
-        else:
-            return ConsoleSMSBackend()
-
-        backend_class = BackendModuleLoader.load_backend_module(
-            settings.get("provider")
-        )
-
-        # Initialize the backend class with any additional arguments
-        # or keyword arguments
-        return backend_class(*args, **kwargs)
+        backend_class = BackendModuleLoader.load_backend_module(settings.get("provider"))
+        return backend_class
