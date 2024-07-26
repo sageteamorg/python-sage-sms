3e7b17a9028f708409e1e2633a004fc769e29196
MohmdFo <mohammad.fotouhi80@gmail.com>
2024-07-26 11:34:42 +0330

    refactor(BackendModuleLoader): ensure load_backend_module return the class obj

--- sage_sms/factory.py
+++ sage_sms/factory.py
@@ -24,11 +24,11 @@ class BackendModuleLoader:
                             break
 
     @staticmethod
-    def load_backend_module(provider: dict):
+    def load_backend_module(provider: dict, base_package: str):
         provider_name = provider.get("NAME")
 
         if not provider_name:
-            raise ValueError("Provider name is missing in settings.")
+            raise ValueError("Provider key is missing in settings.")
 
         if provider_name not in BackendModuleLoader._provider_classname_map:
             raise ValueError(f"Unsupported provider: {provider_name}")
@@ -36,7 +36,9 @@ class BackendModuleLoader:
         if provider_name in BackendModuleLoader._cached_backends:
             return BackendModuleLoader._cached_backends[provider_name]
 
-        provider_class = BackendModuleLoader._provider_classname_map[provider_name]
+        class_name = BackendModuleLoader._provider_classname_map[provider_name]
+        module = import_module(f"{base_package}.{provider_name}")
+        provider_class = getattr(module, class_name)
         BackendModuleLoader._cached_backends[provider_name] = provider_class
         return provider_class
 
@@ -46,11 +48,11 @@ class SMSBackendFactory:
     def __init__(self, settings: dict, base_package: str):
         BackendModuleLoader.discover_backends(base_package)
         self.settings = settings
-
+        self.base_package = base_package
 
     def get_backend(self, *args, **kwargs):
         if self.settings.get("debug", False):
             return ConsoleSMSBackend
 
-        backend_class = BackendModuleLoader.load_backend_module(settings.get("provider"))
+        backend_class = BackendModuleLoader.load_backend_module(self.settings.get("provider"), self.base_package)
         return backend_class
